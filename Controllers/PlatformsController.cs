65126de136e8866b58ff85fcd36471d6b8f22053
Ethan Eils <databasegeek1@gmail.com>
2022-09-30 20:19:20 -0700

    new push

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 using Serilog;
 
 namespace PlatformService.Controllers
@@ -41,6 +42,18 @@ namespace PlatformService.Controllers
       {
         return NotFound();
       }
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+      var platformModel = _mapper.Map<Platform>(platformCreateDto);
+      _repository.CreatePlatform(platformModel);
+      _repository.SaveChanges();
+
+      var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+      return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id}, platformReadDto);
     }
   }
 }
